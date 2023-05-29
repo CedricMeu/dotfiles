b1ed31b1f86102b66ffe984f5c8f9014c98abc4a
CedricMeu <cedric.meukens@icloud.com>
2023-05-29 17:44:36 +0200

    Changed keybindings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,52 +4,53 @@ local WindowManager = require("wm")
 Wm = WindowManager.new()
 Wm:start()
 
-local mods = { "alt" }
+local nav_mods = { "alt" }
+local c_mods = { "ctrl", "alt" }
 
-hs.hotkey.bind(mods, "j", function()
+hs.hotkey.bind(nav_mods, "j", function()
   local layout = Wm:currentLayout()
   local column = layout:getActiveColumn()
   local _ = column:focusNext()
 end)
 
-hs.hotkey.bind(mods, "k", function()
+hs.hotkey.bind(nav_mods, "k", function()
   local layout = Wm:currentLayout()
   local column = layout:getActiveColumn()
   local _ = column:focusPrevious()
 end)
 
-hs.hotkey.bind(mods, "h", function()
+hs.hotkey.bind(nav_mods, "h", function()
   local layout = Wm:currentLayout()
   local _ = layout:focusLeft()
 end)
 
-hs.hotkey.bind(mods, "l", function()
+hs.hotkey.bind(nav_mods, "l", function()
   local layout = Wm:currentLayout()
   local _ = layout:focusRight()
 end)
 
-hs.hotkey.bind(mods, "n", function()
+hs.hotkey.bind(nav_mods, "n", function()
   local layout = Wm:currentLayout()
-
-  if layout:rightmostActive() then
-    local _ = layout:addColumnRight()
-  end
-
   local _ = layout:moveActiveWindowRight()
   Wm:update()
 end)
 
-hs.hotkey.bind(mods, "p", function()
+hs.hotkey.bind(nav_mods, "p", function()
   local layout = Wm:currentLayout()
-
-  if layout:leftmostActive() then
-    local _ = layout:addColumnLeft()
-  end
-
   local _ = layout:moveActiveWindowLeft()
   Wm:update()
 end)
 
-hs.hotkey.bind(mods, "f", function()
+hs.hotkey.bind(nav_mods, "f", function()
   hs.window.focusedWindow():toggleFullScreen()
+end)
+
+hs.hotkey.bind(c_mods, "h", function()
+  Wm:currentLayout():addColumnLeft()
+  Wm:update()
+end)
+
+hs.hotkey.bind(c_mods, "l", function()
+  Wm:currentLayout():addColumnRight()
+  Wm:update()
 end)
